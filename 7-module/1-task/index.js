9cc69c7a57863a771d91ac95a47f4fccd8657f9c
Maxim Dejnew <red41tiger@mail.ru>
2021-06-28 21:15:59 +0700

    Refactored tasks: 6.3; 7.1;

--- 7-module/1-task/index.js
+++ 7-module/1-task/index.js
@@ -4,13 +4,16 @@ export default class RibbonMenu {
   
   constructor(categories) {
     this._categories = categories;
+    this._shift = 350;
+    
     this.elem = document.createElement('div');
     this.elem.classList.add('ribbon');
     this.elem.innerHTML = 
       this._layoutRibbonArrow('left') + 
       this._layoutCategories() + 
       this._layoutRibbonArrow('right'); 
-    this._clickEvents();
+      
+    this._initRibbon();
   }
   
   _layoutRibbonArrow(way) {
@@ -22,53 +25,73 @@ export default class RibbonMenu {
   }
   
   _layoutCategories() {
-    let aList = this._categories.map(item => 
-      `<a href="#" class="ribbon__item" data-id="${ item.id }">${ item.name }</a>` 
-    );
+    let aList = this._categories
+      .map( item => `<a href="#" class="ribbon__item" data-id="${ item.id }">${ item.name }</a>` )
+      .join('');
     return '<nav class="ribbon__inner">' + aList + '</nav>';
   }
   
-  _clickEvents() {
-    const ribbon = this.elem;
-    const ribbonInner = ribbon.querySelector('.ribbon__inner');
-    const scrollWidth = ribbonInner.scrollWidth;
-    const arrowL = ribbon.querySelector('.ribbon__arrow_left');
-    const arrowR = ribbon.querySelector('.ribbon__arrow_right');
-    arrowR.classList.add('ribbon__arrow_visible');
+  _ribbon = suffix => this.elem.querySelector(`.ribbon__${suffix}`);
+  
+  _initRibbon() {
+    this._ribbon('arrow_right').classList.add('ribbon__arrow_visible');
+    this.elem.addEventListener('click', this._onClickEvent);
+  }
+  
+  _onClickEvent = event => {
+    const aim = event.target;
     
-    ribbon.addEventListener('click', e => {
-      const aim = e.target;
-      const showMe = item => {
-        if (!(item.classList.contains('ribbon__arrow_visible')))
-          item.classList.add('ribbon__arrow_visible');
+    if ( aim.classList.contains('ribbon__item') )
+      this._clickOnItem( aim );
+    
+    if ( aim.classList.contains('ribbon__arrow_left') )
+      this._clickOnArrow( -1 );
+    
+    if ( aim.classList.contains('ribbon__arrow_right') )
+      this._clickOnArrow( 1 );
+  }
+  
+  _clickOnItem = (aimNew) => {
+    event.preventDefault();
+    
+    const aimOld = this._ribbon('item_active');
+    
+    if ( aimOld !== aimNew ) {
+      if ( aimOld !== null )
+        aimOld.classList.remove('ribbon__item_active');
+      aimNew.classList.add('ribbon__item_active');
+      
+      this._dispatchCustomEvent(aimNew);
+    }
+  }
+  
+  _clickOnArrow = multiplier => {
+    const shift = this._shift;
+    const arrowStyleSwitch = (scroll, mul, way) => {
+      if ( scroll + ( mul * shift ) <= 1 ) {
+        this._ribbon(`arrow_${way}`).classList.remove('ribbon__arrow_visible');
+      } else {
+        this._ribbon(`arrow_${way}`).classList.add('ribbon__arrow_visible');
       }
-
-      if (aim.classList.contains('ribbon__item')) {
-        e.preventDefault();
-        
-        const itemLastActive = ribbonInner.querySelector('.ribbon__item_active');
-        if (itemLastActive !== null)
-          itemLastActive.classList.remove('ribbon__item_active');
-        aim.classList.add('ribbon__item_active');
-          
-        const itemChosen = new CustomEvent('ribbon-select', {
-          detail: aim.dataset.id,
-          bubbles: true
-        });
-        ribbon.dispatchEvent(itemChosen);
-        
-      } else if (aim.classList.contains('ribbon__arrow_left')) {
-        ribbonInner.scrollBy(-350, 0);
-        ribbonInner.scrollLeft === 0 
-          ? arrowL.classList.remove('ribbon__arrow_visible') 
-          : showMe(arrowR);
-          
-      } else if (aim.classList.contains('ribbon__arrow_right')) {
-        ribbonInner.scrollBy(350, 0);
-        ribbonInner.scrollLeft === scrollWidth 
-          ? arrowR.classList.remove('ribbon__arrow_visible') 
-          : showMe(arrowL);
-      }
+    }
+    let menuWidth = 
+      this._ribbon('inner').scrollWidth -
+      this._ribbon('inner').clientWidth;
+    
+    let scrollLeft = this._ribbon('inner').scrollLeft;
+    let scrollRight = menuWidth - scrollLeft;
+    
+    arrowStyleSwitch(scrollLeft, multiplier, 'left');
+    arrowStyleSwitch(scrollRight, -multiplier, 'right');
+    
+    this._ribbon('inner').scrollBy( shift * multiplier, 0 );
+  }
+  
+  _dispatchCustomEvent = aim => {
+    const itemChosen = new CustomEvent('ribbon-select', {
+      detail: aim.dataset.id,
+      bubbles: true
     });
+    this.elem.dispatchEvent(itemChosen);
   }
 }
